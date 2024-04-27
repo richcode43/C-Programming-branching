8a7d1950c818af94f00935dee4ecf2559a08a807
L3viathanI <tandumtoast@gmail.com>
2024-04-27 17:37:15 +0100

    Finished Merge Array

--- Merge Array(25-04-2024)/main.c
+++ Merge Array(25-04-2024)/main.c
@@ -1,29 +1,92 @@
 #include <stdio.h>
 
- mergeArray()
+int mergeArray(int array1[], int n1, int array2[], int n2, int mergedArray[]) {
+    for (size_t i = 0; i < n1; i++)
+    {
+        mergedArray[i] = array1[i];
+    }
+    
+    for (size_t i = 0; i < n2; i++)
+    {
+        mergedArray[n1 + i] = array2[i];
+    }
+
+    int flag = 1;
+    while (flag) {
+        flag = 0;
+
+        for (size_t i = 0; i < n1 + n2; i++) {
+            if (mergedArray[i] < mergedArray[i + 1]) {
+                int temp = mergedArray[i + 1];
+                mergedArray[i+1] = mergedArray[i];
+                mergedArray[i] = temp;
+
+                flag = 1;
+            }
+        }
+    }
+
+    return 0;
+}
 
 
 int main (){
-    int n;
+    int n1,n2;
 
     printf("Input the number of elements to store in the array: ");
-    scanf("%i", &n);
+    scanf("%i", &n1);
 
-    if (n <= 0)
+    if (n1 <= 0)
     {
         return -1;
     }
     
-    int arr[n];
+    int arr1[n1];
 
-    printf("Input %i number of elements to store in the array.\n", n);
+    printf("Input %i number of elements to store in the array.\n", n1);
 
-    for (size_t i = 0; i < n; i++)
+    for (size_t i = 0; i < n1; i++)
     {
         printf("Element - %i : ", i);
-        scanf("%i", &arr[i]);
+        scanf("%i", &arr1[i]);
     }
 
-    int duplicates = findDuplicates(arr, n);
-    printf("Total number of duplicate elements found in the array is: %i", duplicates);
+    printf("Input the number of elements to store in the other array: ");
+    scanf("%i", &n2);
+
+    if (n2 <= 0)
+    {
+        return -1;
+    }
+    
+    int arr2[n2];
+
+    printf("Input %i number of elements to store in the array.\n", n2);
+
+    for (size_t i = 0; i < n2; i++)
+    {
+        printf("Element - %i : ", i);
+        scanf("%i", &arr2[i]);
+    }
+
+    int arrMerged[n1 + n2];
+    mergeArray(arr1, n1, arr2, n2, arrMerged);
+
+    printf("Original Array 1:");
+    for (size_t i = 0; i < n1; i++)
+    {
+        printf(" %i ", arr1[i]);
+    }
+
+    printf("\nOriginal Array 2:");
+    for (size_t i = 0; i < n2; i++)
+    {
+        printf(" %i ", arr2[i]);
+    }
+
+    printf("\nMerged Array:");
+    for (size_t i = 0; i < n1 + n2; i++)
+    {
+        printf(" %i ", arrMerged[i]);
+    }
 }
